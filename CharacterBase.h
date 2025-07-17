9d567356c686ab98bcd8bb6e1d54f42bee5a5879
ScottyHar <toolmanproductions@gmail.com>
2025-07-17 16:00:46 +1000

    Create CharacterBase.h

---
+++ CharacterBase.h
@@ -0,0 +1,14 @@
+#include "GameFramework/CHaracter.h"
+#include "AuraCharacterbase.Generated.h"
+
+UCLASS(Abstract)
+
+// * constructing variables in the base character that will be in enemy
+* 
+  */
+protected UPROPERTY()
+TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+
+protected UPROPERTY()
+TObjectPtr<UAttributeSet> AttribtueSet;
+
