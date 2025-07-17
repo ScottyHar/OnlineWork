58fd347470148a22fdd9cf7223f3c8de85d4c957
ScottyHar <toolmanproductions@gmail.com>
2025-07-17 16:11:09 +1000

    Update enemy.cpp

--- enemy.cpp
+++ enemy.cpp
@@ -6,6 +6,10 @@ AAuraEnemy::AAuraEneny()
 
 {
   AbilitySystemComponent = CreateDefaultSubObject<UAuraAbilitySystemComponent>("AbilitySystemComponent");
+  AbilitySYstemComponent->SetIsReplicated(true); //this is for when you want it on the server for multiplayer
+  AttributeSet = CreateDefaultSubobject<UAuraAttributeSet>("AttributeSet");
+  
+  
     
 
 
