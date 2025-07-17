ca5efe7b4c2540c67f871b69039cbba1c31c939a
ScottyHar <toolmanproductions@gmail.com>
2025-07-17 16:21:22 +1000

    Update playerstate.cpp

--- playerstate.cpp
+++ playerstate.cpp
@@ -1,6 +1,9 @@
 
-
+AAuraPlayerState::AAuraPlayerState()
 {
+AbilitySystemComponent = CreateDefaultSubObject<UAuraAbilitySystemComponent>("AbilitySystemComponent");
+  AbilitySYstemComponent->SetIsReplicated(true); //this is for when you want it on the server for multiplayer
+  AttributeSet = CreateDefaultSubobject<UAuraAttributeSet>("AttributeSet");
 
-  
+  NetUpdatefrequency = 100.f; //use whenchecking the server for updates
 }
